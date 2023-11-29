5422eeefa2a5bca7d826b9d8f84be4eb105df858
Chico Alive <chicoalive@gmail.com>
2023-11-29 11:51:31 -0300

    Código completo sem generics

--- src/aplicacoes/Programa.java
+++ src/aplicacoes/Programa.java
@@ -1,11 +1,30 @@
 package aplicacoes;
 
+import java.util.Scanner;
+import servicos.ServicoDeImpressao;
 
 public class Programa {
 
-  
     public static void main(String[] args) {
-        
+
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite a quantidade de valores: ");
+
+        ServicoDeImpressao sdi = new ServicoDeImpressao();
+
+        int quantidade = teclado.nextInt();
+
+        for (int i = 0; i < quantidade; i++) {
+            System.out.print((i+1)+"º valor: ");
+            int valor = teclado.nextInt();
+            sdi.addValor(valor);
+        }
+
+        sdi.impressao();
+        System.out.println("Primeiro: " + sdi.primeiro());
+
+        teclado.close();
+
     }
 
 }
--- src/servicos/ServicoDeImpressao.java
+++ src/servicos/ServicoDeImpressao.java
@@ -1,7 +1,33 @@
-
 package servicos;
 
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServicoDeImpressao {
+
+    private List<Integer> lista = new ArrayList<>();
+
+    // Métodos
+    public void addValor(Integer valor) {
+        lista.add(valor);
+    }
+
+    public Integer primeiro() {
+        if (lista.isEmpty()) {
+            throw new IllegalStateException("A lista está vazia!");
+        }
+        return lista.get(0);
+    }
+
+    public void impressao() {
+        System.out.print("[");
+        if (!lista.isEmpty()) {
+            System.out.print(lista.get(0));
+        }
+        for (int i = 1; i < lista.size(); i++) {
+            System.out.print(", "+lista.get(i));
+        }
+        System.out.println("]");
+    }
 
 }
